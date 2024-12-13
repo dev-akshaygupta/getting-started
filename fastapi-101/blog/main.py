06ce5635958c3fe2ed4dd0cfca6bde1c480c55f8
Akshay Gupta <gupta.akshay01@gmail.com>
2024-12-13 06:53:36 -0500

    added data to DB

--- fastapi-101/blog/main.py
+++ fastapi-101/blog/main.py
@@ -1,11 +1,23 @@
-from fastapi import FastAPI
-from . import models, database
-from sqlmodel import SQLModel
+from fastapi import FastAPI, Depends
+from . import database, schemas, models
+from sqlmodel import SQLModel, Session
 
 app = FastAPI()
 
 SQLModel.metadata.create_all(database.engine)
 
+def get_db():
+    db = database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+    
+
 @app.post("/blog")
-def create(request: models.Blog):
-    return {'title':request.title, 'body':request.body} 
+def create_blog(request: schemas.BlogCreate, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
