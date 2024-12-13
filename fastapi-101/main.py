ff1205ea2bceb901fe27a1a8987f1129ac17cb71
Akshay Gupta <gupta.akshay01@gmail.com>
2024-12-13 01:57:26 -0500

    different types of paths

--- fastapi-101/main.py
+++ fastapi-101/main.py
@@ -3,6 +3,8 @@ from fastapi import FastAPI
 # FastAPI instance
 app = FastAPI()
 
+# FastAPI reads routes line by line in the order mentioned below
+
 # Creating Routes
 @app.get("/")   # Decorator to create route
 def hello():
@@ -11,3 +13,25 @@ def hello():
 @app.get("/about")
 def about():
     return {'data':{'about': 'FastAPI hello'}}
+
+@app.get("/bloglist")
+def list_blog():
+    return {'data':'blog list'}
+
+@app.get("/blog/unpublished")
+def unpublished():
+    return {'data': 'All the unpublished'}
+
+@app.get("/blog/{blog_id}")
+def get_blog(blog_id: int):
+    # fetch blog with id
+    return {'data': blog_id}
+
+# @app.get("/blog/unpublished")
+# def unpublished():
+#     return {'data': 'All the unpublished'}
+
+@app.get("/blog/{blog_id}/comments")
+def get_blog(blog_id: int):
+    # fetch comments for a blog with id = blogid
+    return {'data': {'blog id': blog_id, 'comments': 'this is a comment'}}
