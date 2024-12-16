c8eccc970b418f3a2145023deb435168db522c47
Akshay Gupta <gupta.akshay01@gmail.com>
2024-12-16 08:31:36 -0500

    Added exception handling for delete and update

--- fastapi-101/blog/main.py
+++ fastapi-101/blog/main.py
@@ -25,15 +25,19 @@ def create_blog(request: schemas.BlogCreate, db: Session = Depends(get_db)):
 
 @app.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_blog(id, db: Session = Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Blog not found!")
+    blog.delete(synchronize_session=False)
     db.commit()
     return {'Blog is deleted successfully!'}
 
 @app.put("/blog/{id}", status_code=status.HTTP_200_OK)
 def update_blog(id, request: schemas.BlogCreate, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
-    blog.title = request.title
-    blog.body = request.body
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Blog not found!")
+    blog.sqlmodel_update(request)
     db.commit()
     db.refresh(blog)
     return blog
