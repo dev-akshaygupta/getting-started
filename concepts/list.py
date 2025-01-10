30dc64f2b5f8cff7566bd9d0987340d79f0864f2
Akshay Gupta <gupta.akshay01@gmail.com>
2025-01-10 00:01:04 -0500

    list example

---
+++ concepts/list.py
@@ -0,0 +1,34 @@
+from copy import copy, deepcopy
+
+countries = ["United States", "Canada", "Poland", "Germany", "Austria"]
+
+# nations = countries[:]
+# print(id(nations) == id(countries))
+# print(nations[0] == countries[0])
+
+# nations = countries.copy()
+# print(id(nations) == id(countries))
+# print(nations[0] == countries[0])
+
+# nations = copy(countries)
+# print(id(nations) == id(countries))
+# print(nations[0] == countries[0])
+
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# copy_matrix = copy(matrix)
+
+# matrix[0][1] = 200
+# matrix[1][1] = 500
+# matrix[2][1] = 800
+
+# print(matrix, copy_matrix)
+
+# deepcopy_matrix = deepcopy(matrix)
+
+# matrix[0][1] = 200
+# matrix[1][1] = 500
+# matrix[2][1] = 800
+
+# print(matrix, deepcopy_matrix)
+
+print((countries))
